bcf4cd4342a32dbc5491b016be0aef5d6c262f6a
Rafal Jankowski <rafal.jankowski.93@gmail.com>
2017-03-15 22:20:46 +0100

    configuration Spring

---
+++ src/main/java/com/packt/config/MyWebApplicationInitializer.java
@@ -0,0 +1,31 @@
+package com.packt.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        WebApplicationContext context = getContext();
+        servletContext.addListener(new ContextLoaderListener(context));
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
+        dispatcher.addMapping("/");
+    }
+
+    private AnnotationConfigWebApplicationContext getContext() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setConfigLocation("com.packt.config");
+        return context;
+    }
+
+}
